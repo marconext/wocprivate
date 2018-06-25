0a0fd2b69c724c694fd78dc9d5fef309ee9dfcbe
rschindele <rschindele@dxc.com>
2018-06-25 10:15:22 +0200

    Db Creation Script

--- database/create.sql
+++ database/create.sql
@@ -1,10 +1,286 @@
--- Projects
--- Industries
--- Offerings
--- Customers
--- Regions
--- Skills
+GO
+/****** Object:  User [WocDbUser]    Script Date: 25.06.2018 10:13:43 ******/
+CREATE USER [WocDbUser] FOR LOGIN [WocDbUser] WITH DEFAULT_SCHEMA=[dbo]
+GO
+ALTER ROLE [db_datareader] ADD MEMBER [WocDbUser]
+GO
+ALTER ROLE [db_datawriter] ADD MEMBER [WocDbUser]
+GO
+/****** Object:  Table [dbo].[Customers]    Script Date: 25.06.2018 10:13:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Customers](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Customer] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [UQ__Customer__737584F64B3D65F0] UNIQUE NONCLUSTERED 
+(
+	[Name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
 
--- ProjectOfferings
--- ProjectRegions
--- ProjectSkills
+GO
+/****** Object:  Table [dbo].[EmployeeProjectRole]    Script Date: 25.06.2018 10:13:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[EmployeeProjectRole](
+	[EmployeeId] [uniqueidentifier] NOT NULL,
+	[ProjectId] [uniqueidentifier] NOT NULL,
+	[RoleId] [uniqueidentifier] NOT NULL,
+	[Description] [varchar](1024) NULL,
+	[Rate] [money] NOT NULL,
+	[FromDte] [date] NOT NULL,
+	[UntilDte] [date] NULL,
+ CONSTRAINT [PK_EmployeeProjectRole] PRIMARY KEY CLUSTERED 
+(
+	[EmployeeId] ASC,
+	[ProjectId] ASC,
+	[RoleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Employees]    Script Date: 25.06.2018 10:13:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Employees](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[ManagerId] [uniqueidentifier] NULL,
+	[Email] [varchar](50) NULL,
+ CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [UQ__Employee__737584F6C9F9BF73] UNIQUE NONCLUSTERED 
+(
+	[Name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[EmployeeSkills]    Script Date: 25.06.2018 10:13:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[EmployeeSkills](
+	[EmployeeId] [uniqueidentifier] NOT NULL,
+	[SkillId] [uniqueidentifier] NOT NULL,
+	[Maturity] [int] NOT NULL,
+ CONSTRAINT [PK_EmployeeSkill] PRIMARY KEY CLUSTERED 
+(
+	[EmployeeId] ASC,
+	[SkillId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Industries]    Script Date: 25.06.2018 10:13:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Industries](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Name] [varchar](50) NOT NULL
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Offerings]    Script Date: 25.06.2018 10:13:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Offerings](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[KeyNamePath] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Offerings] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[ProjectOfferings]    Script Date: 25.06.2018 10:13:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ProjectOfferings](
+	[ProjectId] [uniqueidentifier] NOT NULL,
+	[OfferingId] [uniqueidentifier] NOT NULL
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[ProjectRegions]    Script Date: 25.06.2018 10:13:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ProjectRegions](
+	[ProjectId] [uniqueidentifier] NOT NULL,
+	[RegionId] [uniqueidentifier] NOT NULL
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[ProjectRoles]    Script Date: 25.06.2018 10:13:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ProjectRoles](
+	[ProjectId] [uniqueidentifier] NOT NULL,
+	[RoleId] [uniqueidentifier] NOT NULL,
+ CONSTRAINT [PK_ProjectRole] PRIMARY KEY CLUSTERED 
+(
+	[ProjectId] ASC,
+	[RoleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Projects]    Script Date: 25.06.2018 10:13:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Projects](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[CustomerId] [uniqueidentifier] NOT NULL,
+	[IndustryId] [uniqueidentifier] NULL,
+	[DXCServices] [ntext] NULL,
+	[Facts] [ntext] NULL,
+	[DXCSolution] [ntext] NULL,
+	[Betriebsleistung] [ntext] NULL,
+ CONSTRAINT [PK_Project] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
+ CONSTRAINT [UQ__Project__C3993C96FB497452] UNIQUE NONCLUSTERED 
+(
+	[Name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[ProjectSkills]    Script Date: 25.06.2018 10:13:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ProjectSkills](
+	[ProjectId] [uniqueidentifier] NOT NULL,
+	[SkillId] [uniqueidentifier] NOT NULL,
+ CONSTRAINT [PK_ProjectSkills] PRIMARY KEY CLUSTERED 
+(
+	[ProjectId] ASC,
+	[SkillId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Regions]    Script Date: 25.06.2018 10:13:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Regions](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[KeyNamePath] [varchar](255) NULL
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Roles]    Script Date: 25.06.2018 10:13:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Roles](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Name] [nchar](10) NOT NULL,
+	[Description] [text] NULL,
+ CONSTRAINT [PK_Role] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Skills]    Script Date: 25.06.2018 10:13:43 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Skills](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Skill] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[EmployeeProjectRole]  WITH CHECK ADD  CONSTRAINT [FK_EmployeeProjectRole_Employee] FOREIGN KEY([EmployeeId])
+REFERENCES [dbo].[Employees] ([Id])
+GO
+ALTER TABLE [dbo].[EmployeeProjectRole] CHECK CONSTRAINT [FK_EmployeeProjectRole_Employee]
+GO
+ALTER TABLE [dbo].[EmployeeProjectRole]  WITH CHECK ADD  CONSTRAINT [FK_EmployeeProjectRole_Project] FOREIGN KEY([ProjectId])
+REFERENCES [dbo].[Projects] ([Id])
+GO
+ALTER TABLE [dbo].[EmployeeProjectRole] CHECK CONSTRAINT [FK_EmployeeProjectRole_Project]
+GO
+ALTER TABLE [dbo].[EmployeeProjectRole]  WITH CHECK ADD  CONSTRAINT [FK_EmployeeProjectRole_Role] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[Roles] ([Id])
+GO
+ALTER TABLE [dbo].[EmployeeProjectRole] CHECK CONSTRAINT [FK_EmployeeProjectRole_Role]
+GO
+ALTER TABLE [dbo].[EmployeeSkills]  WITH CHECK ADD  CONSTRAINT [FK_EmployeeSkill_Employee] FOREIGN KEY([EmployeeId])
+REFERENCES [dbo].[Employees] ([Id])
+GO
+ALTER TABLE [dbo].[EmployeeSkills] CHECK CONSTRAINT [FK_EmployeeSkill_Employee]
+GO
+ALTER TABLE [dbo].[EmployeeSkills]  WITH CHECK ADD  CONSTRAINT [FK_EmployeeSkill_Skill] FOREIGN KEY([SkillId])
+REFERENCES [dbo].[Skills] ([Id])
+GO
+ALTER TABLE [dbo].[EmployeeSkills] CHECK CONSTRAINT [FK_EmployeeSkill_Skill]
+GO
+ALTER TABLE [dbo].[ProjectOfferings]  WITH CHECK ADD  CONSTRAINT [FK_ProjectOfferings_Offerings] FOREIGN KEY([OfferingId])
+REFERENCES [dbo].[Offerings] ([Id])
+GO
+ALTER TABLE [dbo].[ProjectOfferings] CHECK CONSTRAINT [FK_ProjectOfferings_Offerings]
+GO
+ALTER TABLE [dbo].[ProjectOfferings]  WITH CHECK ADD  CONSTRAINT [FK_ProjectOfferings_Projects] FOREIGN KEY([ProjectId])
+REFERENCES [dbo].[Projects] ([Id])
+GO
+ALTER TABLE [dbo].[ProjectOfferings] CHECK CONSTRAINT [FK_ProjectOfferings_Projects]
+GO
+ALTER TABLE [dbo].[ProjectRoles]  WITH CHECK ADD  CONSTRAINT [FK_ProjectRole_Project] FOREIGN KEY([ProjectId])
+REFERENCES [dbo].[Projects] ([Id])
+GO
+ALTER TABLE [dbo].[ProjectRoles] CHECK CONSTRAINT [FK_ProjectRole_Project]
+GO
+ALTER TABLE [dbo].[ProjectRoles]  WITH CHECK ADD  CONSTRAINT [FK_ProjectRole_Role] FOREIGN KEY([RoleId])
+REFERENCES [dbo].[Roles] ([Id])
+GO
+ALTER TABLE [dbo].[ProjectRoles] CHECK CONSTRAINT [FK_ProjectRole_Role]
+GO
+ALTER TABLE [dbo].[Projects]  WITH CHECK ADD  CONSTRAINT [FK_Project_Customer] FOREIGN KEY([CustomerId])
+REFERENCES [dbo].[Customers] ([Id])
+GO
+ALTER TABLE [dbo].[Projects] CHECK CONSTRAINT [FK_Project_Customer]
+GO
