0c7f02455c16560c24f0c17caa29aefe19b7952e
rschindele <rschindele@dxc.com>
2018-07-26 10:26:07 +0200

    .

--- deploy.ng.cmd
+++ deploy.ng.cmd
@@ -91,6 +91,8 @@ echo Handling node.js deployment.
 :: 1. Select node version
 call :SelectNodeVersion
 
+echo Nach SelectNodeVersion
+
 :: 2. Install NPM package (Including dependency packages)
 IF EXIST "%DEPLOYMENT_SOURCE%\package.json" (
   pushd "%DEPLOYMENT_SOURCE%"
